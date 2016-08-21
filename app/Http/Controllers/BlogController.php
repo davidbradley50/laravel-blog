8e5a9fbe69dfd33102f494e44e796abd8c3988a6
David Bradley <davidbradley_ie@hotmail.com>
2016-08-21 14:55:17 +0800

    Cleaned up navigation

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -9,13 +9,23 @@ use App\Models\Post;
 
 class BlogController extends Controller
 {
+
+    public function getIndex()
+    {
+    	// fetch from DB based on slug
+    	$posts = Post::paginate(10);
+
+    	return view('blog.index')->withPosts($posts);
+    	// return the view and pass in the post object
+    }
+
     public function getSingle($slug)
     {
     	// fetch from DB based on slug
     	$post = Post::where('slug',  '=', $slug)->first();
 
-
     	return view('blog.single')->withPost($post);
     	// return the view and pass in the post object
     }
+
 }
